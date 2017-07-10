5caea3c3cce3ca0936e9655f9b32853d72020e30
Boiteux Rémi <boiteux.remi@gmail.com>
2017-07-10 11:32:04 +0200

    CSS modified

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 <head>
 	<link href="styles.css" rel="stylesheet" media="all" type="text/css" />
 	<meta charset="UTF-8">
-	<title>Annuaire</title>
+	<title>Annuaire des développeurs PHP certifiés OpenClassrooms</title>
 </head>
+<h1>Annuaire des développeurs PHP certifiés OpenClassrooms</h1>
 <body>
-</body>
 <?php
 $json_source = file_get_contents('list.json');
 $json_data = json_decode($json_source, true); 
 var_dump($json_data);
 	
-	print_r("$json_data \n");
+	print_r("$json_data");
 
 ?>
+</body>
 </html>
