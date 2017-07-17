c0485495dacdd85db15c7072086ad719420f4aa2
Boiteux Rémi <boiteux.remi@gmail.com>
2017-07-17 13:36:57 +0200

    Profil.html added

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ data_source.forEach(function(item) {
     console.log(item);
     var html = document.createElement("li");
     html.innerHTML = item.nom + ' ' + item.prenom;
+    html.classList.add('élève');
+    html.setAttribute("data-idEleve", item.id);
+    html.addEventListener("click", detectClick);
     ma_liste.appendChild(html);
  });
+
+function detectClick(event){
+	console.log(this);
+	console.log(event);
+	console.log(event.target);
+	var myTarget = event.target;
+	console.log(myTarget.getAttribute("data-idEleve"));
+	window.location = "profil.html" + "#" + myTarget.getAttribute("data-idEleve");
+}
